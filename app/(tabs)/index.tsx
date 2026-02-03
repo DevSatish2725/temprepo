4f99f5a1c740fea5dcd10e437dd5e317526786d9
official-ajeet <m.ajeet15801@gmail.com>
2026-02-03 09:27:48 +0530

    bg changed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ import { Link } from "expo-router";
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: "#2490cf", dark: "#1D3D47" }}
+      headerBackgroundColor={{ light: "#3eb0f3", dark: "#1D3D47" }}
       headerImage={
         <Image
           source={require("@/assets/images/partial-react-logo.png")}
