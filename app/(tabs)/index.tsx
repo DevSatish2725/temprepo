643386d2705846e8027220aea60f5d04f65fb607
DevSatish2725 <mauryasatish146@gmail.com>
2026-02-13 11:45:17 +0530

    Changed bg once again

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ import { ThemedView } from "@/components/themed-view";
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: "#1b7bb4", dark: "#1D3D47" }}
+      headerBackgroundColor={{ light: "#269ee3", dark: "#1D3D47" }}
       headerImage={
         <Image
           source={require("@/assets/images/partial-react-logo.png")}
